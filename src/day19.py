dd26367d0d759b08f2c52fd316185fd2ab2b1814
Phil Hord <phord@purestorage.com>
2023-12-19 17:37:19 -0800

    Day19 part2

--- src/day19.py
+++ src/day19.py
@@ -41,264 +41,82 @@ def game(input, part1):
 
     return (rules, ratings)
 
-def play(rules, ratings):
-    total = 0
-    for rate in ratings:
-        work = "in"
+# arg = (1015,3000)
+# cond = "<1416"
+# return (1015,1416)
+def apply_single(cond, arg, neg):
+    comp = cond[0]
+    val = int(cond[1:])
+    mn,mx = arg
+    if comp == "<" and not neg:
+        mx = min(mx, val)
+    elif comp == ">" and neg:
+        mx = min(mx, val+1)  ## <=
+    elif comp == ">" and not neg:
+        mn = max(mn, val+1)
+    elif comp == "<" and neg:
+        mn = max(mn, val)  ## >=
+    else:
+        print("Unknown condition", comp, " in ", cond)
+        exit(1)
+    if mn < mx:
+        return [(mn,mx)]
+    else:
+        return []
 
-        while work not in ["R", "A"]:
-            rule = rules[work]
-            for r in rule:
-                if ':' not in r:
-                    work = r
-                    break
+# args = [(1,1000), (1015,3000), (3001,4001)]
+# cond = "<1416"
+# return [(1,1000), (1015,1416)]
+def apply_range(cond, args, neg):
+    return [x  for arg in args for x in apply_single(cond, arg, neg)]
 
-                cond, target = r.split(':')
-                var = cond[0]
-                comp = cond[1]
-                val = int(cond[2:])
-                actual = rate[var]
-                if comp == "<":
-                    if actual < val:
-                        work = target
-                        break
-                elif comp == ">":
-                    if actual > val:
-                        work = target
-                        break
-                elif comp == "=":
-                    if actual == val:
-                        work = target
-                        break
-                else:
-                    print("Unknown condition", comp, " in ", r)
-                    exit(1)
-        if work == "A":
-            value = sum([rate[r] for r in rate])
-            total += value
-        elif work == "R":
-            pass
-        else:
-            print("Unknown result", work)
-            exit(1)
-    return total
+# args = ([(1,4001)], [(1,4001)], [(1,4001)], [(1,4001)])
+# cond = "x<1416"
+# return ((1,1416), (1,4001), (1,4001), (1,4001))
+def apply(cond, args, neg):
+    i = "xmas".index(cond[0])
+    new = tuple(apply_range(cond[1:], args[i], neg))
+    return args[:i] + tuple((new,)) + args[i+1:]
 
-# Find all paths that lead to "A" from a given node
-def solve(rules, node, path):
-    paths = []
+# Find all arguments that lead to the "A" node
+def solve(rules, node, args):
     work = node
 
-    if node in ["R", "A"]:
-        return [path + [node]]
+    if node == "R":
+        # yield args
+        return None
+
+    if node == "A":
+        yield args
+        return
 
     rule = rules[work]
     for r in rule:
         if ':' not in r:
-            paths.extend(solve(rules, r, path))
+            yield from solve(rules, r, args)
             break
 
         cond, target = r.split(':')
-        next = solve(rules, target, path + [cond])
+        yield from solve(rules, target, apply(cond, args, False))
+        args = apply(cond, args, True)
 
-        paths.extend(next)
+    return
 
-    return paths
-
-def combos(path):
-    map = {'x': (1,4001), 'm': (1,4001), 'a': (1,4001), 's': (1,4001)}
-
-    for cond in path[:-1]:
-        var = cond[0]
-        comp = cond[1]
-        val = int(cond[2:])
-        mn,mx = map[var]
-        if comp == "<":
-            mx = min(mx, val)
-        elif comp == ">":
-            mn = max(mn, val+1)
-        elif comp == "=":
-            mn = val
-            mx = val
-        else:
-            print("Unknown condition", comp, " in ", cond)
-            exit(1)
-        map[var] = (mn,mx)
-
-    return [map[var] for var in "xmas"]
-
-# Split two ranges into three disjoint ranges
-def split(r1, r2):
-    mn1,mx1 = r1
-    mn2,mx2 = r2
-
-    # Common range
-    c1 = max(mn1, mn2)
-    c2 = min(mx1, mx2)
-    if c1 <= c2:
-        left = [ a for a in [(mn1, c1), (c2, mx1), (c1,c2+1)] if a[0] <= a[1]]
-        return left
-    else:
-        return [r1]
-
-def disjoint(p1, p2):
-    left = split(p1[0], p2[0])
-    if len(p1) == 1:
-        return [[a] for a in left]
-
-    lr = disjoint(p1[1:], p2[1:])
-    a = []
-    for l in left:
-        for l2 in lr:
-            a.append([l] + l2)
-    return a
-
-
-# a = [(1,5),(2,4), (5,12), (12,20)]
-# b = [(3,7),(1,3), (3,6), (1,8)]
-# for i,j in zip(a,b):
-#     print(i, j, " ==> ", split(i,j))
-
-# x,y = disjoint(a,b)
-# for i in x:
-#     print(i)
-# for i in y:
-#     print(i)
-
-
-# exit(1)
+def count_intervals(i):
+    return sum([mx-mn for mn,mx in i])
 
 def count(p1):
     total = 1
-    for i in range(4):
-        mn,mx = p1[i]
-        total *= (mx-mn)
+    for iv in p1:
+        total *= count_intervals(iv)
     return total
 
-def show(p):
-    print(count(p), p)
-
-def disjoint_1xn(p1, paths):
-    pp = set([tuple(p1)])
-
-    for path in paths:
-        next = set()
-        for p in pp:
-            left = disjoint(p, path)
-            for a in left:
-                next.add(tuple(a))
-        pp = next
-
-    # ## Validate pp
-    # a = count(p1)
-    # b = 0
-    # for p in pp:
-    #     b += count(p)
-    # assert a == b
-
-    return pp
-
-def disjoint_all(paths):
-    pp = set()
-    for i in range(len(paths)):
-        pp |= disjoint_1xn(paths[i], paths)
-    return pp
-
-# a = [(1,5),(2,4), (5,12), (13,17)]
-# b = [(3,7),(1,3), (3,6), (1,8)]
-
-# show(a)
-# show(b)
-# c = disjoint_1xn(a, [b])
-# for i in c:
-#     show(i)
-# print()
-# d = disjoint_all([a,b])
-# total = 0
-# for i in d:
-#     show(i)
-#     total += count(i)
-# print(total)
-# exit(1)
-
-# a = [(2663, 4000), (1, 4000), (1, 2005), (1, 1350)] #['s<1351', 'a<2006', 'x>2662', 'A']
-# b = [(1, 4000), (1, 4000), (1, 2005), (1, 1350)]    #['s<1351', 'a<2006', 'R']
-# c = [(1, 4000), (1, 4000), (1, 4000), (1, 1350)]    #['s<1351', 'A']
-
-# show(a)
-# show(b)
-# show(c)
-# print()
-
-
-def disjoiner(path, vertices):
-    # print(path)
-    rang = [sorted([x for x in s if p[0] <= x <= p[1]]) for p, s in zip(path, vertices)]
-    # print(rang)
-    rang = [[(a,b) for a,b in zip(x[0:-1], x[1:])] for x in rang]
-    # print(rang)
-    return product(*rang)
-
-# input = getem()
-
-g = game(input, True)
-g = play(*g)
-print("Part 1: ", g)
-
+input = getem()
 
 rules, _ = game(input, True)
-paths = solve(rules, "in", [])
-# for p in paths:
-#     print(combos(p), p)
-# print()
-
-comb = [combos(p) for p in paths]
-
-# get a map of every parameter range for every path
-vertices = []
-for i in range(4):
-    ps = set()
-    for p in comb:
-        ps.add(p[i][0])
-        ps.add(p[i][1])
-    vertices.append(ps)
-# print(vertices)
-
-# for p in comb:
-#     print(len(disjoiner(p, vertices)))
-#     print(disjoiner(p, vertices))
-    # print(sorted(disjoint_1xn(p, comb)))
-
-# dj = disjoint_all(comb)
-
-## Validate all resulting paths are disjoint
-# for p in dj:
-#     for q in dj:
-#         if p != q:
-#             z = disjoint_1xn(p, [q])
-#             assert len(z) == 1
-#             assert p in z
-
-print("Length of paths: ", len(paths))
-print("Length of vertices: ", [len(x) for x in vertices])
-
-seen = set()
 total = 0
-for i, (p, c) in enumerate(zip(paths, comb)):
-    print(p, c)
-    dis = disjoiner(c, vertices)
-
-    for x in dis:
-        x = tuple(x)
-        # print(count(x), x)
-        if p[-1] == "A":
-            # remove paths already seen
-            if x not in seen: #dis -= seen
-                total += count(x)
-        seen.add(x)
+for args in solve(rules, "in", (((1,4001),), ((1,4001),), ((1,4001),), ((1,4001),))):
+    print(count(args), args)
+    total += count(args)
 print(total)
-
-
-# 206016000000000 - 167409079868000 = 38606920132000
-# print(([combos(p) for p in paths if p[-1] == "A"]))
-
-# print("Part 2: ", g)
+exit(1)
