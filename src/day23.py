64b89591ef3598050d2fc174d09a53480b829e70
Phil Hord <phord@purestorage.com>
2023-12-22 23:49:01 -0800

    Day 23 Part2 (30 seconds)

--- src/day23.py
+++ src/day23.py
@@ -1,5 +1,3 @@
-import re, collections, itertools as it
-
 input='''#.#####################
 #.......#########...###
 #######.#########.#.###
@@ -23,10 +21,6 @@ input='''#.#####################
 #.###.###.#.###.#.#v###
 #.....###...###...#...#
 #####################.#'''
-from functools import lru_cache
-
-
-# Brick = collections.namedtuple('Brick', ['id', 'cells', 'minZ'])
 
 input = open('src/day23.txt').read()
 
@@ -46,7 +40,6 @@ def dump(grid, path):
                 print(grid[y][x], end='')
         print()
 
-@lru_cache(maxsize=10000, typed=False)
 def neighbors(grid, x,y):
     nbors = []
     for dx,dy in ((0,1),(1,0),(0,-1),(-1,0)):
@@ -56,96 +49,46 @@ def neighbors(grid, x,y):
                 nbors.append((nx,ny))
     return tuple(nbors)
 
-def trails(grid):
-    # find every trail segment's start, end and length
-    # where a trail segment has no branches
+def measure(edges, start, head):
+    count = 1
+    while len(edges[head]) == 2:
+        count += 1
+        next = [n for _,n in edges[head] if n != start]
+        assert len(next) == 1
+        next = next[0]
+        start = head
+        head = next
+    return (count, head)
 
-    seen = set()
+
+def trails(grid):
     edges = {}
     for y in range(height):
         for x in range(width):
             if grid[y][x] in '.<>^v':
-                edges[(x,y)] = neighbors(grid, x,y)
+                edges[(x,y)] = [(1,n) for n in neighbors(grid, x,y)]
 
-    trailheads = {}
-    for k, v in edges.items():
-        if len(v) > 2:
-            for n in v:
-                if len(edges[n]) == 2:
-                    # Found a trailhead
-                    trailheads[n] = k
-
-    trail = {}
-    for t, start in trailheads.items():
-        if t in trail:
-            continue
-        tt = [t]
-        next = [n for n in edges[t] if len(edges[n]) == 2]
-        while len(next) == 1:
-            if len(edges[next[0]]) > 2: break
-            tt.append(next[0])
-            next = [n for n in edges[next[0]] if n not in tt]
-
-        # print(t, tt)
-        last = tt[-1]
-        assert last in trailheads or len(edges[last]) == 1
-        if not next:
-            next = [last]
-            assert len(edges[last]) == 1
-
-        trail[t] = (next[0], last, len(tt))
-        trail[last] = (start, t, len(tt))
-
-    graph = {}
+    # Collapse all trail segments into a single edge
+    newedges = {}
     for k,v in edges.items():
-        for n in v:
-            if len(edges[n]) > 2:
-                if k not in trail and n not in trail:
+        if len(v) != 2:
+            newedges[k] = [measure(edges, k, n[1]) for n in v]
 
-        assert v[0] in trail
-        assert v[1] in trail
-
-    return trail
-
-# print(len(t))
-# for k,v in t.items():
-#     print(k, v)
-
-# print(trails(grid))
-# exit(1)
+    return newedges
 
 def dfs(grid, trails, start, end):
     seen = set([start])
     stack = [(start, 0, seen)]
     mx = 0
     while stack:
-        # print(len(stack), end=' ')
-        (x,y),dist, seen = stack.pop()
-        seen = set(seen)
+        pos, dist, seen = stack.pop()
 
-        # # Clear the screen and home cursor
-        # print("\033[2J\033[H", end='')
-        # dump(grid, seen)
-
-        if (x,y) == end:
+        if pos == end:
             mx = max(mx, dist)
-            if mx == dist:
-                print("End at", dist)
-            # dump(grid, seen)
-            # return dist
-        if (x,y) in trails:
-            (nx,ny),(x0, y0), l = trails[(x,y)]
-            # print(trails[(x,y)])
-            if (nx,ny) not in seen:
-                seen.add((nx,ny))
-                # seen.add(x0,y0)
-                stack.append(((nx,ny), dist+l, tuple(seen)))
-        else:
-            for nx,ny in neighbors(grid, x,y):
-                if (nx,ny) not in seen:
-                    seen.add((nx,ny))
-                    stack.append(((nx,ny), dist+1, tuple(seen)))
+        for d, next in trails[pos]:
+            if next not in seen:
+                stack.append((next, dist+d, seen | set([next])))
 
-    return mx-1
+    return mx
 
 print( dfs(grid, trails(grid), start, end))
